1e892b56ebb17a2f24baa49732a76955beeaa938
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-02-09 14:55:21 -0800

    pc - update default properties so no TEST_PROPERTIES needed

--- src/main/java/edu/ucsb/menumanager/DemoApplication.java
+++ src/main/java/edu/ucsb/menumanager/DemoApplication.java
@@ -1,5 +1,8 @@
 package edu.ucsb.menumanager;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +14,15 @@ import org.springframework.context.annotation.PropertySource;
 public class DemoApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    SpringApplication app = new SpringApplication(DemoApplication.class);
+    Map<String, Object> defaults = new HashMap<String, Object>();
+    defaults.put("app.namespace", "https://FAKE-NAMESPACE.example.org");
+    defaults.put("app.admin.emails", "phtcon@ucsb.edu");
+    defaults.put("auth0.domain", "YOUR-AUTH0-TENANT.us.auth0.com");
+    defaults.put("security.oauth2.resource.id", defaults.get("app.namespace"));
+    defaults.put("security.oauth2.resource.jwk.keySetUri", "https://" + defaults.get("auth0.domain") + "/.well-known/jwks.json");
+    app.setDefaultProperties(defaults);
+    app.run(args);
   }
 
 }
